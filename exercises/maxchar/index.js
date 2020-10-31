9a4c028f1c893a515bba6d58a5856bfd4acf8140
AlexL70 <alexander.levinson.70@gmail.com>
2020-10-31 17:26:03 +0200

    18. Max Chars Problem - my first solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let keys = [];
+    let values = [];
+    str.split('').forEach(element => {
+        let index = keys.findIndex(el => el === element);
+        if(index < 0) {
+            keys.push(element);
+            values.push(1);
+        } else {
+            values[index]++;
+        }
+    });
+    maxValue = 0;
+    maxIndex = -1;
+    values.forEach((element, index) => {
+        if(element > maxValue){
+            maxValue = element;
+            maxIndex = index;
+        }
+    });
+    return keys[maxIndex];
+}
 
 module.exports = maxChar;
